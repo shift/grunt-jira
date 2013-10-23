91442e981e1b318598481aa91c4dbf1fcaecb84e
Vincent Palmer <vincent.palmer@odobo.com>
2013-10-23 11:43:08 +0200

    Basic create version call.

--- tasks/jira.js
+++ tasks/jira.js
@@ -8,43 +8,38 @@
 
 'use strict';
 
+var JiraApi = require('jira').JiraApi;
+
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
-  grunt.registerMultiTask('jira', 'JIRA intergration for Grunt', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
+  grunt.registerTask('jira-create-version', 'JIRA intergration for Grunt', function() {
+    grunt.config.requires('grunt-jira.protocol', 'grunt-jira.host', 'grunt-jira.port', 'grunt-jira.user', 'grunt-jira.password', 'grunt-jira.project_key');
     var options = this.options({
       punctuation: '.',
       separator: ', '
     });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+    var version = grunt.config.get('grunt-jira.version') || grunt.package.version;
+    var jira = new JiraApi(
+      grunt.config.get('grunt-jira.protocol'),
+      grunt.config.get('grunt-jira.host'),
+      grunt.config.get('grunt-jira.port'),
+      grunt.config.get('grunt-jira.user'),
+      grunt.config.get('grunt-jira.password'),
+      '2',
+      true
+    );
+    jira.createVersion({
+      'project': grunt.config.get('grunt-jira.project_key'),
+      'version': version,
+      'name': "Automated grunt build",
+      'description': "This is an automated grunt build.",
+      'released': false,
+      'archived': false
+    }, function(callback) {
+      grunt.log.ok(callback);
+      grunt.log.ok("JIRA version: " + version + " has been created.");
     });
+
   });
 
 };
